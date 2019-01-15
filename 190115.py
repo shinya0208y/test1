6f5bcc21f34196379e036a620d6dcefba8b92ae3
shinya0208y <shinya0208y@gmail.com>
2019-01-16 00:17:17 +0900

    190115-1

---
+++ 190115.py
@@ -0,0 +1,12 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Scraper:
+    def __init__(self,site):
+        self.site = site
+
+    def scrape(self):
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+        
+    
