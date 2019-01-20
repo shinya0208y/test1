35affe9b460cc7439a112bb217a075c1b26cccbd
shinya0208y <shinya0208y@gmail.com>
2019-01-20 23:58:37 +0900

    190120-1

---
+++ 190120-1.py
@@ -0,0 +1,31 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        last = len(self.items) -1
+        return self.items[last]
+
+    def size(self):
+        return len(self.items)
+
+stack = Stack()
+
+for i in range(0,6):
+    stack.push(i)
+
+print(stack.peek())
+print(stack.size())
+
+
+        
+    
